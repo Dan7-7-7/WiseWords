1bcaf43266a66a5862515650b4350fa18c4eeae2
Dan7-7-7 <dannyranson19@gmail.com>
2022-04-08 17:56:52 +0100

    planned app and set up components and containers

---
+++ src/containers/QuotesBox.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const QuotesBox = () => {
+
+
+    return (
+        <h1>this is the container for the quotes and the select inputs</h1>
+    )
+}
+
+export default QuotesBox;
+
+// heading COMPONENT
+// three options in a flex row: CONTAINER
+//          choose a quotifier (author) => list of quotes, click through one by one with a "next" button COMPONENT
+            // what's on your mind today? (list of keyword topics) => as above COMPONENT
+            // just gimme a random quote! => single quote COMPONENT
+// the quote box - displaying one quote at a time. COMPONENT
+// (in the first two select cases) a next button IN BOX COMPONENT
+// optional footer (or header): quote of the day COMPONENT
